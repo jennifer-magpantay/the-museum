fea894361a2062a6edd4eac6a6ffd15030983f71
Jennifer Magpantay <jennifer.magpantay@gmail.com>
2022-07-01 18:07:28 +0200

    chore: Set project structure and style

---
+++ project/src/App.tsx
@@ -0,0 +1,12 @@
+// import {} from "phosphor-react";
+// <Heart size={32} color="hotpink" weight="fill" />
+
+import "./style/ap.scss";
+
+export const App = () => {
+  return (
+    <>
+      <h1>Hello Stranger</h1>
+    </>
+  );
+};
---
+++ project/src/components/SocialMediaBox.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SocialMediaBox = () => {
+  return (
+    <div>SocialMediaBox</div>
+  )
+}
